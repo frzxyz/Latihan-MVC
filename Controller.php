95d34301c553b512582440fca9fe55bd980cb87f
frzxyz <accsmurfer@gmail.com>
2023-08-26 15:55:12 +0700

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -1,6 +1,6 @@
 <?php
-include "Model.php";
-include "View.php";
+include 'Model.php';
+include 'View.php';
 
 class Controller
 {
@@ -27,53 +27,53 @@ class Controller
             $this->read();
         }
     }
-}
-
-function read(){
-    $mRead = new Model();
-    $data = $mRead->read();
-
-    $vBeranda = new View();
-    $vBeranda = beranda($data);
-}
-
-function create()
-{
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $mCreate = new Model();
-        $nim = $_POST['nim'];
-        $nama = $_POST['nama'];
-        $angkatan = $_POST['angkatan'];
-        $prodi = $_POST['prodi'];
-        $kelas = $_POST['kelas'];
-        $mCreate->insert($nim, $nama, $angkatan, $prodi, $kelas);
-
+    function read(){
+        $mRead = new Model();
+        $data = $mRead->read();
+    
+        $vBeranda = new View();
+        $vBeranda->beranda($data);
     }
-    header("Location: index.php");
-}
-
-function update()
-{
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $mUpdate = new Model();
-        $nim_baru = $_POST['nim_baru'];
-        $nim_lama = $_POST['nim_lama'];
-        $nama = $_POST['nama'];
-        $angkatan = $_POST['angkatan'];
-        $prodi = $_POST['prodi'];
-        $kelas = $_POST['kelas'];
-        $mUpdate->update($nim_baru, $nim_lama, $nama, $angkatan, $prodi, $kelas);
-
+    
+    function create()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $mCreate = new Model();
+            $nim = $_POST['nim'];
+            $nama = $_POST['nama'];
+            $angkatan = $_POST['angkatan'];
+            $prodi = $_POST['prodi'];
+            $kelas = $_POST['kelas'];
+            $mCreate->insert($nim, $nama, $angkatan, $prodi, $kelas);
+    
+        }
+        header("Location: index.php");
     }
-    header("Location: index.php");
+    
+    function update()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $mUpdate = new Model();
+            $nim_baru = $_POST['nim_baru'];
+            $nim_lama = $_POST['nim_lama'];
+            $nama = $_POST['nama'];
+            $angkatan = $_POST['angkatan'];
+            $prodi = $_POST['prodi'];
+            $kelas = $_POST['kelas'];
+            $mUpdate->update($nim_baru, $nim_lama, $nama, $angkatan, $prodi, $kelas);
+    
+        }
+        header("Location: index.php");
+    }
+    
+    function delete()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $mDelete = new Model();
+            $id = $_POST['id'];
+            $mDelete->delete($id);
+        }
+        header("Location: index.php");
+    }
 }
 
-function delete()
-{
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $mDelete = new Model();
-        $id = $_POST['id'];
-        $mDelete->delete($id);
-    }
-    header("Location: index.php");
-}
